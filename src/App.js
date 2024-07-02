73d19e877967445cbc25a9d282984eed05dcdfc2
Mat Gargano <mgargano@gmail.com>
2024-07-02 12:00:06 -0400

    maybe shallow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
+import "./App.css";
 
 function App() {
   const [contacts, setContacts] = useState([]);
@@ -10,15 +11,15 @@ function App() {
       Papa.parse(file, {
         header: true,
         skipEmptyLines: true,
-        complete: function (results) {
+        complete: async function (results) {
           setContacts(results.data);
+          await copyHtmlContent(); // Automatically copy HTML after parsing
         },
       });
     }
   };
 
   function nl2br(text) {
-    // Split the text on new lines and filter out empty strings if they exist
     return text.split("\n").map((item, index) => (
       <React.Fragment key={index}>
         {item}
@@ -40,15 +41,8 @@ function App() {
 
   return (
     <div className="App">
-      {contacts.length === 0 && (
-        <>
-          <h1>Upload CSV File</h1>
-          <input type="file" accept=".csv" onChange={handleFileChange} />
-        </>
-      )}
-      {contacts.length > 0 && (
-        <button onClick={copyHtmlContent}>Copy HTML</button>
-      )}
+      <h1>Upload CSV File</h1>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
       <div className="COPYABLE-HTML">
         <div className="agents-listing">
           {contacts.map((contact, index) => (
@@ -68,7 +62,6 @@ function App() {
                 </p>
                 {contact.Cell && (
                   <p className="no-margin agent-single__phone">
-                    <span className="agent__single__icon-phone agent__single__icon"></span>
                     <a href={`tel:+1-${contact.Cell.replace(/[^0-9]/g, "")}`}>
                       {contact.Cell}
                     </a>
@@ -76,7 +69,6 @@ function App() {
                 )}
                 {contact.Email && (
                   <p className="no-margin agent-single__email">
-                    <span className="agent__single__icon-envelope agent__single__icon"></span>
                     <a href={`mailto:${contact.Email}`}>{contact.Email}</a>
                   </p>
                 )}
@@ -91,7 +83,6 @@ function App() {
                       <span class="hide-button">Hide Bio &rarr;</span>
                       <span class="show-button">Show Bio &rarr;</span>
                     </span>
-
                     <div className="my-5 bio">{nl2br(contact.Bio)}</div>
                   </div>
                 )}
