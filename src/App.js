616679c771c8be6e90bbf8ba5a996551d3e4af99
Mat Gargano <mgargano@gmail.com>
2024-07-02 10:26:22 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Papa from "papaparse";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+          setContacts(results.data);
+        },
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {contacts.length === 0 && (
+        <>
+          <h1>Upload CSV File</h1>
+          <input type="file" accept=".csv" onChange={handleFileChange} />
+        </>
+      )}
+      <div className="agents-listing">
+        {contacts.map((contact, index) => (
+          <div className="agent-single" key={index}>
+            <div class="agent-single__left"></div>
+            <div class="agent-single__right">
+              <p className="no-margin agent-single__name">
+                {contact["First Name"]} {contact["Last Name"]}
+              </p>
+              {contact.Cell && (
+                <p className="no-margin agent-single__phone">
+                  <span class="agent__single__icon-phone agent__single__icon"></span>
+                  <a href={`tel:+1-${contact.Cell.replace(/[^0-9]/g, "")}`}>
+                    {contact.Cell}
+                  </a>
+                </p>
+              )}
+              {contact.Email && (
+                <p className="no-margin agent-single__email">
+                  <span class="agent__single__icon-envelope agent__single__icon"></span>
+                  <a href={`mailto:${contact.Email}`}>{contact.Email}</a>
+                </p>
+              )}
+              {contact.WorkFax && (
+                <p className="no-margin agent-single__note">
+                  {contact.WorkFax}
+                </p>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
